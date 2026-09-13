440d36dd74af84a6f0ce11793a153b1224c92d8b
vaishnavidommeti <vaishnavidommeti@gmail.com>
2026-09-13 14:06:39 +0530

    configure django for production

--- student_portal/settings.py
+++ student_portal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7yx^^m!$*(s58n4fu_+f*ijq=1$=!@rk+@$a#w=3ib&+#7-%bo'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
